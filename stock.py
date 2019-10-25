e99c45784b3f802c758fadfb0ad13fc4aa7fd23d
peihuan <peihuan@sensetime.com>
2019-10-26 00:23:34 +0800

    update

--- stock.py
+++ stock.py
@@ -16,28 +16,32 @@ def get_html(t_url):
 def list_concept(t_url):
     html = get_html(t_url)
     soup = BeautifulSoup(html, 'html.parser')
-    cate_items = soup.find_all(href=re.compile("http://q.10jqka.com.cn/gn/detail/code"))
-    for cate in cate_items:
-        print(cate['href'])
-        print(cate.text)
-
-    board_aside = soup.find('div', {'class': 'board-txt board-aside'}).find('p')
-    remark = board_aside.text
-    print(remark)
+    # cate_items = soup.find_all(href=re.compile("http://q.10jqka.com.cn/gn/detail/code"))
+    # for cate in cate_items:
+    #     print(cate['href'])
+    #     print(cate.text)
+    #
+    # board_aside = soup.find('div', {'class': 'board-txt board-aside'}).find('p')
+    # remark = board_aside.text
+    # print(remark)
     board_hq = soup.find('div', {'class': 'board-hq'})
-    name = board_hq.find("h3").text
-    print(name[:-6])
-    value = board_hq.find('span', {'class': 'board-xj arr-fall'})
-    print(value)
+    print(board_hq.h3.span.text)
+    span = board_hq.find("span", class_="board-xj arr-fall")
+    print(span)
+    print(board_hq.p)
+    # for child in board_hq.h3.contents:
+    #     print(child)
+    # print(board_hq.h3.span.string)
+    # print(board_hq.span.string)
 
     # print(board_hq.find('span', {'class': 'board-xj arr-fall'}).text)
-    print(board_hq.find('p').text)
+    # print(board_hq.find('p').text)
 
-    dl = soup.find('div', {'class': 'board-infos'}).find_all('dl')
-    for d in dl:
-        print(type(d))
-        # print(d['dt'].text+' '+d['dd'].text)
-    print(dl)
+    # dl = soup.find('div', {'class': 'board-infos'}).find_all('dl')
+    # for d in dl:
+    #     # print(type(d))
+    #     print(d.dt.text+' '+d.dd.text)
+    # print(dl)
 
 
 if __name__ == '__main__':
